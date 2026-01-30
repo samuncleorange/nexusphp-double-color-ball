a18f8c865dd42c359e425976ac70ef6885fbd806
Shu <shu@example.com>
2026-01-30 18:20:48 +0800

    fix: Add Create button to Periods list page

--- src/Filament/Resources/PeriodResource/Pages/ListPeriods.php
+++ src/Filament/Resources/PeriodResource/Pages/ListPeriods.php
@@ -2,10 +2,18 @@
 
 namespace NexusPlugin\DoubleColorBall\Filament\Resources\PeriodResource\Pages;
 
+use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 use NexusPlugin\DoubleColorBall\Filament\Resources\PeriodResource;
 
 class ListPeriods extends ListRecords
 {
     protected static string $resource = PeriodResource::class;
+
+    protected function getHeaderActions(): array
+    {
+        return [
+            Actions\CreateAction::make(),
+        ];
+    }
 }
